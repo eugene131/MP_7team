7a16e794d772bab702feacab20e787afc1799b47
Minseo Jeong <jeongminseo999@gmail.com>
2022-11-24 19:56:31 +0900

    ReportPage xml

---
+++ movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
@@ -0,0 +1,11 @@
+package com.example.movie_line
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RePortActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_page)
+    }
+}
