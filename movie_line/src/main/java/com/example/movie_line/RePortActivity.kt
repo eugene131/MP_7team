284fdbe68d3ab458adfb45666d35b1910fc8ffb7
Minseo Jeong <jeongminseo999@gmail.com>
2022-12-18 19:52:17 +0900

    add reportpage_alarm

--- movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
+++ movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class RePortActivity : AppCompatActivity() {
@@ -19,6 +20,7 @@ class RePortActivity : AppCompatActivity() {
         }
     }
     fun goToMain(view: View) {
+        Toast.makeText(this, "신고되었습니다", Toast.LENGTH_SHORT).show()
         val intent = Intent(this, MainPage::class.java)
         startActivity(intent)
     }
