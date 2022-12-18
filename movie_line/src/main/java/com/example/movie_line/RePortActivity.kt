31e72648ebc8e550cff95b751543b719e413d289
Minseo Jeong <jeongminseo999@gmail.com>
2022-12-18 19:38:07 +0900

    modify ReportPage

--- movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
+++ movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
@@ -1,11 +1,25 @@
 package com.example.movie_line
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class RePortActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_page)
+        val button: Button = findViewById(R.id.checkbox5)
+        val text: EditText = findViewById(R.id.editText)
+
+        button.setOnClickListener {
+            text.setText("신고사유를 작성해주세요")
+        }
+    }
+    fun goToMain(view: View) {
+        val intent = Intent(this, MainPage::class.java)
+        startActivity(intent)
     }
 }
