9507332fc5b6cd8c2c58bb873ddb57f03dc6e9a5
Minseo Jeong <jeongminseo999@gmail.com>
2022-12-19 19:57:47 +0900

    add reportPage

--- movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
+++ movie_line/src/main/java/com/example/movie_line/RePortActivity.kt
@@ -12,16 +12,18 @@ class RePortActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_page)
-        val button: Button = findViewById(R.id.checkbox5)
-        val text: EditText = findViewById(R.id.editText)
+        
+        val button: Button = findViewById(R.id.checkbox5) //기타 checkButton
+        val text: EditText = findViewById(R.id.editText) //사유작성란 editText
 
         button.setOnClickListener {
-            text.setText("신고사유를 작성해주세요")
+            text.setText("신고사유를 작성해주세요") //기타 체크하면 구체적인 사유 작성란
         }
     }
+    //신고하기 버튼 누르면 "신고되었습니다"알림창과 메인화면으로 이동
     fun goToMain(view: View) {
-        Toast.makeText(this, "신고되었습니다", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this, MainPage::class.java)
+        Toast.makeText(this, "신고되었습니다", Toast.LENGTH_SHORT).show() //알림창
+        val intent = Intent(this, MainPage::class.java) //메인페이지 화면전환
         startActivity(intent)
     }
 }
