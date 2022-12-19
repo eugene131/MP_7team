83c754001b0dfd60e1af50af3e8dd57d898203c4
cascade <rainy1405@naver.com>
2022-12-19 21:06:03 +0900

    스크롤뷰 추가

---
+++ reademe/src/main/java/com/example/reademe/Reademe.kt
@@ -0,0 +1,11 @@
+package com.example.reademe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reademe : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reademe)
+    }
+}
