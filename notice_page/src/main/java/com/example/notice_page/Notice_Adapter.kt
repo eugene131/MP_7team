881b59724dfb76af086846998df1f4b141abe953
cascade <rainy1405@naver.com>
2022-12-19 19:52:29 +0900

    명대사+알림 레이아웃 수정, 주석 추가

--- notice_page/src/main/java/com/example/notice_page/Notice_Adapter.kt
+++ notice_page/src/main/java/com/example/notice_page/Notice_Adapter.kt
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.notice_page.databinding.NoticeItemMainBinding
 
 
-//noticetime 설정해야함
+//알림 내용, 시간이 담긴 리스트를 받아옴
 class Notice_Adapter(val datas: MutableList<String>, val times:MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     class NoticeMyViewHolder(val binding: NoticeItemMainBinding): RecyclerView.ViewHolder(binding.root)
     override fun getItemCount(): Int =datas.size
@@ -21,14 +21,11 @@ class Notice_Adapter(val datas: MutableList<String>, val times:MutableList<Strin
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding=(holder as NoticeMyViewHolder).binding
-        //각 댓글 내용+시간 세팅
+        //알림에 들어갈 내용 데이터 세팅
         binding.noticeitemData.text=datas[position]
         binding.noticetime.text=times[position]
-        //아이템 클릭리스너
-        binding.noticeItemRoot.setOnClickListener{
+        binding.noticeItemRoot.setOnClickListener{//각 아이템을 클릭시 댓글 페이지로 이동하게됨
             Log.d("test","item root click: $position")
-            //position별로 각각 레이아웃 넘어가게 설정하면되는가
-            //넘어갈 레이아웃을 담을 리스트를 만들어서 쓰면 될듯?
             val intent: Intent = Intent( binding.noticeItemRoot?.context,TestLayout::class.java)
             //startActivity(intent)
             ContextCompat.startActivity( binding.noticeItemRoot.context, intent,null)
--- notice_page/src/main/java/com/example/notice_page/notice_page.kt
+++ notice_page/src/main/java/com/example/notice_page/notice_page.kt
@@ -15,25 +15,22 @@ class notice_page : AppCompatActivity() {
 
         //이 부분 조절해서 발생시키면 될듯 - 알림 클릭시 페이지 넘어가도록 구현해야함
         //알림 타입(댓글/신고) + 댓글 내용 + 시간 정도 조절하면 될듯
-        val datas = mutableListOf<String>()
-        val times = mutableListOf<String>()
-        fun add_comment(data:String, time:String){
+        val datas = mutableListOf<String>()//알림 내용을 저장하는 리스트
+        val times = mutableListOf<String>()//알림 발생 시간을 저장하는 리스트
+        fun add_notice(data:String, time:String){//알림 내용을 세팅하는 함수
             datas.add(data)
             times.add(time)
         }
-        add_comment("댓글 내용1","2022-12-11")
-        add_comment("댓글 내용2","2022-12-11")
-        /*
-        for (i in 1..10) {
-            datas.add("item $i")
-        }
-        */
+        //테스트를 위한 알림 추가
+        add_notice("댓글 내용1","2022-12-11")
+        add_notice("댓글 내용2","2022-12-11")
+
         //리스트에 있는 것들을 다 보여주는 방식.
-        //데이터와 타임 리스트를 따로 둘 게 아니라 구조체처럼 정의해서 써야될 것 같은데        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = Notice_Adapter(datas,times)
         binding.recyclerView.adapter = Notice_Adapter(datas,times)
         binding.recyclerView.addItemDecoration(
-            DividerItemDecoration(this,
-            LinearLayoutManager.VERTICAL)
+            DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
         )
     }
 }
